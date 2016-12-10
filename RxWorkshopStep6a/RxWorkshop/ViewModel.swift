bb5ec30dd99590f5430a7e51491c5307f0dad1c2
Khoi Truong Minh <khoi.truongminh@gmail.com>
2016-12-10 12:53:54 +0700

    Add Step 6 solution, step 6a

---
+++ RxWorkshopStep6a/RxWorkshop/ViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  ViewModel.swift
+//  RxWorkshop
+//
+//  Created by Khoi Truong Minh on 12/7/16.
+//  Copyright © 2016 Khoi Truong Minh. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct ViewModel: ViewModelType {
+
+    let query: Variable<String> = Variable<String>("")
+    var minimumStars: Variable<Int> = Variable<Int>(500)
+
+    let searchDescription: Observable<String>
+    let results: Observable<String>
+    let showNoStarWarning: Observable<Void>
+    let showEasterEggAlert: Observable<Void>
+
+    init() {
+        searchDescription = query.asObservable()
+            .map(toSearchDescription)
+            .observeOn(MainScheduler.instance)
+
+        results = MockGitHubAPI
+            .getRepositories()
+            .map(toString)
+
+        showNoStarWarning = minimumStars.asObservable()
+            .filter { $0 < 10 }
+            .map { _ in }
+            .take(1)
+
+        showEasterEggAlert = query.asObservable()
+            .filter { $0.lowercased() == "rxswift" }
+            .map { _ in }
+            .take(1)
+    }
+
+}
+
+fileprivate func toSearchDescription(query: String) -> String {
+    return "Searching for \(query.uppercased())"
+}
+
+fileprivate func toString(repos: [Repository]) -> String {
+    return repos.reduce("", { (result, repo) -> String in
+        return result + "\n"
+            + "📦 " + repo.owner.login + "/" + repo.name + "\n"
+            + "⭐️ " + String(repo.starCount) + "\n"
+            + "     " + repo.description + "\n" + "\n\n"
+            + "----------------------" + "\n"
+    })
+}
